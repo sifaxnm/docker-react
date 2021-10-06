043b78ebfefb3db8a706a4ebee40786b6e0b703a
sifaxnm <hs_nait_mouloud@esi.dz>
2021-10-06 15:53:58 +0200

    added travis file

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          hello
+        <h1 className="App-title">Welcome to React</h1>
       </header>
     </div>
   );
